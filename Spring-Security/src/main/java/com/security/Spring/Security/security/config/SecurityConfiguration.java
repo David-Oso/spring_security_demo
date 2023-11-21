54058205b410d7cd9ed759faec47298430304d78
David <osodavid001@gmail.com>
2023-11-21 16:11:32 +0100

    security config

--- Spring-Security/src/main/java/com/security/Spring/Security/security/config/SecurityConfiguration.java
+++ Spring-Security/src/main/java/com/security/Spring/Security/security/config/SecurityConfiguration.java
@@ -1,12 +1,79 @@
 package com.security.Spring.Security.security.config;
 
+import com.security.Spring.Security.security.filter.ProjectAuthorizationFilter;
+import com.security.Spring.Security.security.util.ProjectAuthenticationEntryPoint;
+import com.security.Spring.Security.security.util.WhiteList;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+import static com.security.Spring.Security.appUser.model.enums.Permission.*;
+import static com.security.Spring.Security.appUser.model.enums.Role.ADMIN;
+import static com.security.Spring.Security.appUser.model.enums.Role.MANAGER;
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.http.HttpMethod.DELETE;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration {
+    private final ProjectAuthorizationFilter authorizationFilter;
+    private final ProjectAuthenticationEntryPoint authenticationEntryPoint;
+    private final LogoutHandler logoutHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling
+                                .authenticationEntryPoint(authenticationEntryPoint))
+                .sessionManagement(sessionManagement ->
+                        sessionManagement
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(
+                                WhiteList.freeAccess())
+                        .permitAll()
+                        .requestMatchers(WhiteList.swagger())
+                        .permitAll()
+
+                        .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
+                        .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
+                        .requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
+                        .requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
+                        .requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
+
+                        .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
+                        .requestMatchers(GET, "/api/v1/admin/**").hasAuthority(ADMIN_READ.name())
+                        .requestMatchers(POST, "/api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
+                        .requestMatchers(PUT, "/api/v1/admin/**").hasAuthority(ADMIN_UPDATE.name())
+                        .requestMatchers(DELETE, "/api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())
+
+                        .anyRequest()
+                        .authenticated())
+                .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(logout ->
+                        logout.logoutUrl("/api/v1/auth/logout")
+                                .addLogoutHandler(logoutHandler)
+                                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext()))
+                .build();
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
---
+++ Spring-Security/src/main/java/com/security/Spring/Security/security/util/WhiteList.java
@@ -0,0 +1,43 @@
+package com.security.Spring.Security.security.util;
+
+import lombok.Getter;
+@Getter
+public class WhiteList {
+    public static String[] freeAccess() {
+        return new String[]{
+                "/user/register",
+                "/user/verify",
+                "/user/login",
+                "/user/resend_verification_mail",
+        };
+    }
+
+    public static String[] swagger() {
+        return new String[]{
+                "/v2/api-docs",
+                "/v3/api-docs",
+                "/v3/api-docs/**",
+                "/swagger-resources",
+                "/swagger-resources/**",
+                "/configuration/ui",
+                "/configuration/security",
+                "/swagger-ui/**",
+                "webjars/**",
+                "/swagger-ui.html"
+        };
+    }
+
+//    private static final HandlerMappingIntrospector handlerMappingIntrospector = new HandlerMappingIntrospector();
+//    public static MvcRequestMatcher freeAccess() {
+//    return new MvcRequestMatcher(handlerMappingIntrospector,
+//            "/user/register|/user/verify|/user/login|/user/resend_verification_mail");
+//    }
+//
+//    public static MvcRequestMatcher swagger() {
+//        return new MvcRequestMatcher(handlerMappingIntrospector,
+//                "/v2/api-docs|/v3/api-docs|/v3/api-docs/**|/swagger-resources|"+
+//                "/swagger-resources/**|/configuration/ui|/configuration/security|/swagger-ui/**|"+
+//                "/webjars/**|/swagger-ui.html"
+//        );
+//    }
+}
